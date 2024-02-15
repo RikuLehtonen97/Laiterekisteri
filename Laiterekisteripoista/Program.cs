f7c4782a45c807ade25b271939ea98a28e7e7385
RikuLehtonen97 <47364@edu.raseko.fi>
2024-02-15 11:27:25 +0200

    v1 15/02/2024

---
+++ Laiterekisteripoista/Program.cs
@@ -0,0 +1,99 @@
+﻿using Laitekirjasto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laitekirjasto
+{
+    // Yleinen laiteluokka, yliluokka tietokoneille, tableteille ja puhelimille
+
+    class Device
+    {
+        // Luodaan kenttä (field) name, esitellään (define) ja annetaan arvo (set initial value)
+        string name = "Uusi laite";
+        // Luodaan kenttää vastaava ominaisuus (property) Name ja sille asetusmetodi set ja lukumetodi get. Ne voi kirjoittaa joko yhdelle tai useammalle riville
+        public string Name { get { return name; } set { name = value; } }
+
+        string purchaseDate = "1.1.1900";
+        public string PurchaseDate { get { return purchaseDate; } set { purchaseDate = value; } }
+
+        // Huomaa jälkiliite d (suffix)
+        double price = 0.0d;
+        public double Price { get { return price; } set { price = value; } }
+
+        int warranty = 12;
+        public int Warranty { get { return warranty; } set { warranty = value; } }
+
+        // Konstruktori eli olionmuodostin (constructor) ilman argumentteja
+        public Device()
+        {
+
+        }
+
+        // Konstruktori nimi-argumentilla
+        public Device(string name)
+        {
+            this.name = name;
+        }
+
+        // Konstruktori kaikilla argumenteilla
+        public Device(string name, string purchaseDate, double price, int warranty)
+        {
+            this.name = name;
+            this.purchaseDate = purchaseDate;
+            this.price = price;
+            this.warranty = warranty;
+        }
+
+
+
+    }
+
+    class Computer : Device
+    {
+        // kentät
+        string processorType;
+        public string ProcessorType { get { return processorType; } set {  processorType = value; } }
+        
+        int amountRam;
+        public int AmountRam { get { return amountRam; } set {  amountRam = value; } }
+
+        int storageCapacity;
+        public int StorageCapacity { get { return storageCapacity; } set { storageCapacity = value; } }
+
+        //konstructorit
+        public Computer() : base() 
+            {}
+        
+
+        // ominaisuudet
+    }
+
+    // Pääohjelman luokka, josta tulee Program.exe
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ohjelman varsinaiset toiminnot tapahtuvat täällä
+            // Ohjelma kysyy käyttäjältä tietoja laitteista ja
+            // vastaamalla kysymyksiin tiedot tallennetaan muuttujiin.
+
+            // Luodaan uusi laite Device-luokasta
+
+            Device laite = new Device("Opekone");
+            Console.WriteLine("Laitteen nimi on: " + laite.Name);
+            Console.WriteLine("Ostopäivä oli: " + laite.PurchaseDate);
+
+             //luodaan uusi tietokone joka perii laiteluokan (device) ominaisuudet ja metodit
+            Computer tietokone1 = new Computer();
+
+            // 
+            Console.WriteLine("uuudestaan uudestaan luodaan tietokone nimeltä" +  tietokone1.Name);
+
+            // Pidetään ikkuna auki, kunnes käyttäjä painaa <enter>
+            Console.ReadLine();
+        }
+    }
+}
